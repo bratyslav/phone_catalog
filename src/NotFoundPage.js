f3172d431dd07ba399db33e53d46a308adcf48d1
bratyslav <beginner.altavista@gmail.com>
2019-08-08 16:43:51 +0300

    fixed not found page

--- src/NotFoundPage.js
+++ src/NotFoundPage.js
@@ -1,9 +1,7 @@
 import React from 'react';
 
-const NotFoundPage = () => {
-  return (
-    <h1 className="not-found-message">Page Not Found</h1>
-  );
-};
+const NotFoundPage = () => (
+  <h1 className="not-found-message">Page Not Found</h1>
+);
 
 export default NotFoundPage;
